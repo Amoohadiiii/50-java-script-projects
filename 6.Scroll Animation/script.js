c11c7a16684599148d3dcb942e568a9fd566dce2
hadipouranian <hadi.pouranian@gmail.com>
2021-07-13 13:04:20 +0800

    small projects to practice html , css and js

---
+++ 6.Scroll Animation/script.js
@@ -0,0 +1,20 @@
+"use strict";
+
+const boxes = document.querySelectorAll(".box");
+
+window.addEventListener("scroll", checkBoxes);
+
+checkBoxes();
+
+function checkBoxes() {
+  const triggerBottom = window.innerHeight * 0.73;
+
+  boxes.forEach((box) => {
+    const boxTop = box.getBoundingClientRect().top;
+    if (boxTop < triggerBottom) {
+      box.classList.add("show");
+    } else {
+      box.classList.remove("show");
+    }
+  });
+}
